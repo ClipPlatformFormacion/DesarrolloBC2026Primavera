c59e5bfa2ffeadd713afbd831468d6ff9e65a750
Cristina Nicolàs <cristina.nicolas@clipplatform.com>
2026-04-20 15:20:48 +0200

    Documentación

---
+++ Documentación/ContenedorBC.ps1
@@ -0,0 +1,10 @@
+﻿#Install-Module -Name BcContainerHelper -Force
+
+$artifactUrl = Get-BCArtifactUrl -country es -select Latest -version 27
+
+
+$UserName = 'Alumno'
+$Password = 'Formacion2026'
+$credential = New-Object pscredential $UserName, (ConvertTo-SecureString -String $Password -AsPlainText -Force)
+
+New-BcContainer -containerName BC27CU5ES -artifactUrl $artifactUrl -auth UserPassword -Credential $credential -accept_eula -useBestContainerOS -updateHosts -includeTestToolkit -includeTestLibrariesOnly
