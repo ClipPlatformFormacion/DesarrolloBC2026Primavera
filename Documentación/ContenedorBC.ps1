a7f98c81ea802b6e87de28d46a39120df0b30a95
Cristina Nicolàs <cristina.nicolas@clipplatform.com>
2026-05-27 15:43:14 +0200

    creación contenedor para BC28.1

--- Documentación/ContenedorBC.ps1
+++ Documentación/ContenedorBC.ps1
@@ -1,10 +1,10 @@
 ﻿#Install-Module -Name BcContainerHelper -Force
 
-$artifactUrl = Get-BCArtifactUrl -country es -select Latest -version 27
+$artifactUrl = Get-BCArtifactUrl -country es -select Latest -version 28.1
 
 
 $UserName = 'Alumno'
 $Password = 'Formacion2026'
 $credential = New-Object pscredential $UserName, (ConvertTo-SecureString -String $Password -AsPlainText -Force)
 
-New-BcContainer -containerName BC27CU5ES -artifactUrl $artifactUrl -auth UserPassword -Credential $credential -accept_eula -useBestContainerOS -updateHosts -includeTestToolkit -includeTestLibrariesOnly
+New-BcContainer -containerName BC28CU1ES -artifactUrl $artifactUrl -auth UserPassword -Credential $credential -accept_eula -useBestContainerOS -updateHosts -includeTestToolkit -includeTestLibrariesOnly
